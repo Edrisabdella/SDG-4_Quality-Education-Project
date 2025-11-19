2399f8f30affa2eccf0d8b2f2bc25f07e78ed349
Edrisabdella <edrisabdella178@gmail.com>
2025-11-20 02:30:44 +0300

    Delete backend directory

--- backend/routes/analyticsRoutes.js
+++
@@ -1,9 +0,0 @@
-// Analytics routes
-const express = require('express');
-const { getAnalytics } = require('../controllers/analyticsController');
-
-const router = express.Router();
-
-router.route('/').get(getAnalytics);
-
-module.exports = router;
